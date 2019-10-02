171b15ff78ec28603f0cea915c256455812b8ad4
May <may159@csiro.au>
2019-10-02 16:06:54 +1000

    add a for loop for(i in 1:length(sensors) to check data in each individual sensor.

--- scripts/check_sensor_files.R
+++ scripts/check_sensor_files.R
@@ -1,8 +1,9 @@
-#sensor list 
+#sensor list then summarise data in the files, from Sensor DB. 
 library(tidyverse)
 library(lubridate)
 library(readxl)
 
+#Read files in a folder
 
 canopy_file_names <- list.files(path = "data/warren",
                                 recursive = T,
@@ -11,6 +12,7 @@ canopy_file_names <- list.files(path = "data/warren",
 
 
 
+#create a dataframe reading in the files. 
 
 df <- data_frame(filename = canopy_file_names) %>% 
   mutate(file_contents = map(filename, ~ read_csv(file.path(.),
@@ -22,12 +24,7 @@ df_unnested <- unnest(df) %>%
   mutate(UID = basename(filename)) %>% 
   mutate(UID = str_extract(UID, "PHEN\\d{3}"))
 
-
-min_max_all <- df_unnested %>%  
-  summarise(max_date = max(Timestamp, na.rm = TRUE),
-            min_date = min(Timestamp, na.rm = TRUE),
-            max = max(Value, na.rm = TRUE),
-            min = min(Value, na.rm = TRUE))
+#Summarise with UID from file
 
 min_max <- group_by(df_unnested, UID) %>% 
   summarise(max_date = max(Timestamp, na.rm = TRUE),
@@ -35,20 +32,65 @@ min_max <- group_by(df_unnested, UID) %>%
             max = max(Value, na.rm = TRUE),
             min = min(Value, na.rm = TRUE))
 
+#create a tibble to work with. 
 
-
-  as.tibble() %>% 
-mutate(CanopyTemp = as.numeric(CanopyTemp),
-       Datetime = ymd_hms(Datetime, tz = "Australia/Sydney")) %>%
-  select(ID, CanopyTemp, Datetime)
+SensorExtract <-   as.tibble(df_unnested) %>% 
+mutate(Value = as.numeric(Value),
+       Timestamp = ymd_hms(Timestamp, tz = "Australia/Sydney")) %>%
+  select(UID, Value, Timestamp)
 #------------
 str(SensorExtract)
 
+ggplot(SensorExtract, aes(x = Timestamp, y = Value))+
+  geom_line()+
+  facet_wrap(~UID)
+
+
+#view each sensor in a "for" loop for a closer look at each individual sensor. 
+
+sensors <- unique(SensorExtract$UID)
+
+plotUID <- sensors[i]
+
+i = 2
+for (i in 1:length(sensors)){
+  
+  plotUID <- sensors[i]
+  
+  g1 <- ggplot(filter(SensorExtract, UID == plotUID), aes(x = Timestamp, y = Value))+
+    geom_line()+
+    labs(
+      x = "Time",              # x axis title
+      y = "CT",   # y axis title
+      title = plotUID)      # main title of figure
+  # theme(axis.text.x=element_blank(), axis.ticks.x=element_blank())
+  
+  print(g1)
+  
+}
+ggplot(filter(SensorExtract, UID == plotUID), aes(x = Timestamp, y = Value))+
+  geom_line()+
+  labs(
+    x = "Time",              # x axis title
+    y = "CT",   # y axis title
+    title = plotUID)      # main title of figure
+    theme(axis.text.x=element_blank(), axis.ticks.x=element_blank())
+  
+  plot(data = az.countries, aes(x = year, y = lifeExp, color=continent)) +
+  geom_line() + facet_wrap( ~ country) +
+  labs(
+    x = "Year",              # x axis title
+    y = "Life expectancy",   # y axis title
+    title = "Figure 1",      # main title of figure
+    color = "Continent"      # title of legend
+  ) +
+  theme(axis.text.x=element_blank(), axis.ticks.x=element_blank())
+  
 
 # create a table of all the SensorIDs in the data file
 Sensors <- SensorExtract %>% distinct(ID) 
 
-SensorExtract <- filter(SensorExtract, str_detect(ID, "^20..$")) %>% 
+SensorExtract <- filter(SensorExtract, str_detect(UID, "^...$")) %>% 
   filter(Datetime < Sys.time()) %>% 
   #filter(Datetime >= "2017/12/01"& Datetime <= "2018/12/01") %>% 
   filter(between(CanopyTemp, 5, 50))
@@ -65,7 +107,7 @@ ggplot(SensorExtract, aes(x = Datetime, y = CanopyTemp))+
 # use a for loop to go filter the data by each sensor ID 
 for (i in 1:nrow(Sensors)) {
   
-  SensorID <- Sensors$ID[i]
+  SensorID <- Sensors$UID[i]
   
   BySensor <- SensorExtract %>% 
     filter(ID == SensorID) %>%
