54b0eaa08a6e789796aafac9627f731481fb173a
May <may159@csiro.au>
2019-11-19 11:06:00 +1100

    making lists and mins and maxs, all the good things

--- scripts/check_sensor_files.R
+++ scripts/check_sensor_files.R
@@ -34,7 +34,7 @@ min_max <- group_by(df_unnested, UID) %>%
 
 #create a tibble to work with. 
 
-SensorExtract <-   as.tibble(df_unnested) %>% 
+SensorExtract <-   as_tibble(df_unnested) %>% 
 mutate(Value = as.numeric(Value),
        Timestamp = ymd_hms(Timestamp, tz = "Australia/Sydney")) %>%
   select(UID, Value, Timestamp)
@@ -76,46 +76,30 @@ ggplot(filter(SensorExtract, UID == plotUID), aes(x = Timestamp, y = Value))+
     title = plotUID)      # main title of figure
     theme(axis.text.x=element_blank(), axis.ticks.x=element_blank())
   
-  plot(data = az.countries, aes(x = year, y = lifeExp, color=continent)) +
-  geom_line() + facet_wrap( ~ country) +
-  labs(
-    x = "Year",              # x axis title
-    y = "Life expectancy",   # y axis title
-    title = "Figure 1",      # main title of figure
-    color = "Continent"      # title of legend
-  ) +
-  theme(axis.text.x=element_blank(), axis.ticks.x=element_blank())
-  
+
 
 # create a table of all the SensorIDs in the data file
-Sensors <- SensorExtract %>% distinct(ID) 
+sensortable <- SensorExtract %>% distinct(UID) %>% 
 
 SensorExtract <- filter(SensorExtract, str_detect(UID, "^...$")) %>% 
-  filter(Datetime < Sys.time()) %>% 
-  #filter(Datetime >= "2017/12/01"& Datetime <= "2018/12/01") %>% 
-  filter(between(CanopyTemp, 5, 50))
+  filter(Timestamp < Sys.time()) %>% 
+  #filter(Datetime >= "2017/12/01"& Datetime <= "2018/12/01") %>% #Use this to filter date time by selecting date.
+  filter(between(Value, 5, 50)) %>% 
+  
 
 
 # create a table of all the SensorIDs in the data file
-Sensors <- SensorExtract %>% distinct(ID) 
-
-
-ggplot(SensorExtract, aes(x = Datetime, y = CanopyTemp))+
-  geom_line()+
-  facet_wrap(~ID)
 
 # use a for loop to go filter the data by each sensor ID 
-for (i in 1:nrow(Sensors)) {
+for (i in 1:nrow(sensortable)) {
   
-  SensorID <- Sensors$UID[i]
+  SensorID <- sensortable$UID[i] %>% 
   
   BySensor <- SensorExtract %>% 
-    filter(ID == SensorID) %>%
-    select(-ID) %>% # remove ID column as the output csv will contain this informaiton
-    #filter(Datetime >= "2017/12/01"& Datetime <= "2018/12/01") %>% 
-    #filter(CanopyTemp >= 5 & CanopyTemp <= 50) %>% 
-    mutate(Datetime = floor_date(Datetime,unit = "minutes")) %>%  #round to nearest minute
-    
+    filter(UID == SensorID) %>%
+    select(-UID) %>% # remove ID column as the output csv will contain this informaiton
+    mutate(Timestamp = floor_date(Timestamp,unit = "minutes")) %>%  #round to nearest minute
+  
     
     #clean up sensorDat, aggregate to minutes
     #filter for unique values
@@ -128,3 +112,4 @@ for (i in 1:nrow(Sensors)) {
   
   try(write_csv(mutate(BySensor,Datetime=format(Datetime, "%F %H:%M:%S")),
                 SaveFileName))
+}
